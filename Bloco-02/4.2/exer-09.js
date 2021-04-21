b48d584d3010d6656a01d6986dfe51a3de78abb4
Ghislaine <glenox79@gmail.com>
2021-04-21 07:59:02 -0300

    Adicionando projetos

--- Bloco-02/4.2/exer-09.js
+++
@@ -1,9 +0,0 @@
-let list = [];
-let result = 0;
-
-for (let i = 1; i < 25; i += 1){
-    list.push(i);
-    result /= list[i]
-}console.log(result);
-
-
--- Bloco-02/4.3/exer-02.js
+++
@@ -1,9 +0,0 @@
-let n = 5;
-let symbol = '*';
-let line = '';
-
-
-for (let i = 1; i <= n; i += 1) {
-    line += symbol;
-    console.log(line);
-}
